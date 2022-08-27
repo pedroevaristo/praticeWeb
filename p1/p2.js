81ffb26d988d8525efc7f880b5e2d2f223751c77
pedroevaristo <hattoriminamoto@gmail.com>
2022-08-27 11:10:03 -0300

    27.08.22

--- p1/p2.js
+++ p1/p2.js
@@ -5,22 +5,22 @@ var Elementoutput = document.getElementById('output');
 Elementbutton.addEventListener("click", function () {
     if (Elementinput.value) {
         console.log(Elementinput.value)
-        Elementinput.value = "";
-    
-    if (Elementinput.value >= 5 && Elementinput.value <= 11) {
         
-        Elementoutput.innerHTML = "Infantil"
-        console.log("Infantil");
-    
-    } if (Elementinput.value >= 12 && Elementinput.value <= 17) {
-    
-        console.log("Juvenil");
-    
-    } else {
-    
-        console.log("Adultos");
-    
+        if (Elementinput.value >= 5 && Elementinput.value <= 11) {
+
+            Elementoutput.innerHTML = "Infantil";
+            console.log("Infantil");
+
+        } if (Elementinput.value >= 12 && Elementinput.value <= 17) {
+            Elementoutput.innerHTML = "Juvenil";
+            console.log("Juvenil");
+
+        } else {
+            Elementoutput.innerHTML = "Adulto";
+            console.log("Adultos");
+
+        }
+        Elementinput.value = "";
     }
-}
 
 })
