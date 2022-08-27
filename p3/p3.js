35837a52c3910d88d753a3fe14e418c08ea7adbb
pedroevaristo <hattoriminamoto@gmail.com>
2022-08-27 15:17:12 -0300

    27.08.22

---
+++ p3/p3.js
@@ -0,0 +1,6 @@
+const input1 = document.getElementById();
+const input2 = document.getElementById();
+const input3 = document.getElementById();
+const result = document.getElementById();
+
+/*tentar colocar em vetores as informações sendo colocado */
