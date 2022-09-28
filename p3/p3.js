3f2d1b0755357beb82d64e20392b796350401935
pedroevaristo <hattoriminamoto@gmail.com>
2022-09-27 21:46:04 -0300

    27.09.22

--- p3/p3.js
+++ p3/p3.js
@@ -1,6 +1,7 @@
 const input1 = document.getElementById();
 const input2 = document.getElementById();
 const input3 = document.getElementById();
+const input4 = document.getElementById();
 const result = document.getElementById();
 
-/*tentar colocar em vetores as informações sendo colocado */
+/*tentar colocar em vetores as informações sendo colocado. Colocar a quantida de alunos que quer colocar */
