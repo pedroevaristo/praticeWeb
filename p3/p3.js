491b491da3c9c092837180ca138555cc8ff6fbeb
pedroevaristo <hattoriminamoto@gmail.com>
2022-09-29 22:04:44 -0300

    29.09.22

--- p3/p3.js
+++ p3/p3.js
@@ -1,7 +1,18 @@
-const input1 = document.getElementById();
-const input2 = document.getElementById();
-const input3 = document.getElementById();
-const input4 = document.getElementById();
-const result = document.getElementById();
+const input1 = document.getElementById('numStudent');
+const input2 = document.getElementById('numNote1');
+const input3 = document.getElementById('numNote2');
+const input4 = document.getElementById('numNote3');
+const result = document.getElementById('result');
 
-/*tentar colocar em vetores as informações sendo colocado. Colocar a quantida de alunos que quer colocar */
+/*tentar colocar em vetores as informações 
+sendo colocado. Colocar a quantida de alunos que quer colocar */
+
+Elementbutton.addEventListener("click", function(){
+    result.value = document.getElementById('result') (input2 + input3 + input4)/3;
+    console.log(result.value)
+    result.value = "";
+})
+
+/*function calculate(){
+   const result = document.getElementById('result') (input2 + input3 + input4)/3;
+}*/
