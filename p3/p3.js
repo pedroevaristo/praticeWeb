c2b79b369c2c051f7acb15aeb67c98960c8d0a8c
pedroevaristo <hattoriminamoto@gmail.com>
2022-10-03 18:31:43 -0300

    3.10.22

--- p3/p3.js
+++ p3/p3.js
@@ -1,18 +1,18 @@
-const input1 = document.getElementById('numStudent');
-const input2 = document.getElementById('numNote1');
-const input3 = document.getElementById('numNote2');
-const input4 = document.getElementById('numNote3');
-const result = document.getElementById('result');
-
+var quantStudent = document.getElementById('numStudent');
+var inNumber1 =(document.getElementById('numNote1').value);
+var inNumber2 = (document.getElementById('numNote2').value);
+var inNumber3 = (document.getElementById('numNote3').value);
 /*tentar colocar em vetores as informações 
 sendo colocado. Colocar a quantida de alunos que quer colocar */
+/*p3.js:10 Uncaught ReferenceError: input2 is not defined
+at HTMLButtonElement.<anonymous> (p3.js:10:45)*/
+buttonSend.addEventListener("click", function(){
 
-Elementbutton.addEventListener("click", function(){
-    result.value = document.getElementById('result') (input2 + input3 + input4)/3;
-    console.log(result.value)
-    result.value = "";
-})
+document.getElementById('result').value  = (input2 + input3 + input4)/3;
 
+result.value = "";
+
+} )
 /*function calculate(){
    const result = document.getElementById('result') (input2 + input3 + input4)/3;
 }*/
