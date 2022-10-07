5e5db0989cd6fea22d0f0412ef25045296d62765
pedroevaristo <hattoriminamoto@gmail.com>
2022-10-07 16:48:49 -0300

    7.10.22

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ` Ler a repeitos: `
 
  [ link para direcionar sobre The DOM ](https://www.freecodecamp.org/news/the-javascript-dom-a-practical-tutorial/)
+
+----
+
+## Informações que tiraram as minhas duvidas das questões anteriores.
+
+- 1 https://stackoverflow.com/questions/71008084/how-can-i-console-log-my-sum-from-the-function-below
+
+- 2 https://stackoverflow.com/questions/25544000/how-to-calculate-sum-using-javascript
+
--- p3/p3.js
+++ p3/p3.js
@@ -1,19 +1,26 @@
 var quantStudent = document.getElementById('numStudent');
-var inNumber1 =(document.getElementById('numNote1'));
-var inNumber2 = (document.getElementById('numNote2'));
-var inNumber3 = (document.getElementById('numNote3'));
+var inNumber1 =document.getElementById('numNote1');
+var inNumber2 = document.getElementById('numNote2');
+var inNumber3 = document.getElementById('numNote3');
+
+
+buttonSend.addEventListener("click", function(){
+   result = Number(inNumber1.value)+Number(inNumber2.value)+Number(inNumber3.value);
+
+   console.log(result/3);
+   document.getElementById('result').innerHTML = result/3;
+} )
+
+/*
+Da pra usar dentro da function, no caso 'buttonSend.addEventListener'
+
+var quantStudent = document.getElementById('numStudent');
+   var inNumber1 =parseInt(document.getElementById('numNote1').value);
+   var inNumber2 = parseInt(document.getElementById('numNote2').value);
+   var inNumber3 = parseInt(document.getElementById('numNote3').value);*/
+
+
 /*tentar colocar em vetores as informações 
 sendo colocado. Colocar a quantida de alunos que quer colocar */
 /*p3.js:10 Uncaught ReferenceError: input2 is not defined
 at HTMLButtonElement.<anonymous> (p3.js:10:45)*/
-buttonSend.addEventListener("click", function(){
-console.log((inNumber1 + inNumber2 + inNumber3)/3);
-
-//document.getElementById('result').value  = (input2 + input3 + input4)/3;
-
-result.value = "";
-
-} )
-/*function calculate(){
-   const result = document.getElementById('result') (input2 + input3 + input4)/3;
-}*/
