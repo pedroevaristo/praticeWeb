7f9eb1253baee24d2734523c0158b18b89bc5726
pedroevaristo <hattoriminamoto@gmail.com>
2022-11-13 20:05:13 -0300

    13.11.22

---
+++ p4/p4.js
@@ -0,0 +1,15 @@
+var inputInfor = document.getElementById("input");
+
+var functionButton = document.getElementById("buttonSend");
+console.log("tete");
+functionButton.addEventListener("click", function () {
+    for () {
+        if (inputInfor.value % 2 == 0) {
+            console.log("É par")
+
+        } else {
+
+        }
+    }
+
+})
