0c65a089438858dc74dcd6454aadf928dbee3ff4
pedroevaristo <hattoriminamoto@gmail.com>
2022-11-21 21:26:07 -0300

    21.11.22

--- p4/p4.js
+++ p4/p4.js
@@ -1,6 +1,24 @@
+array = [];
+
 var text = document.getElementById("textInformation");
+
 var button = document.getElementById("buttonInformation");
+
+var show = document.getElementById("showInformation");
 
 button.addEventListener("click", function(){
     
+    array.push(text.value);
+    
+    text.value = "";
+
+    var pointOrder = document.createElement("ol");
+
+    for(var i = 0; i < array.length; i +=1 ){
+
+
+
+    }
+
+
 })
